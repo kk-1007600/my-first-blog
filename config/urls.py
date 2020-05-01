63d106912a65a526670dd6bac57d928f74067c6f
unknown <kk-1007600>
2020-05-01 16:12:43 +0900

    修正

--- config/urls.py
+++ config/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
 
 # 管理サイトの見出しを変更可能
 #  タイトル；タイトルタグで使用
-admin.site.site_title = 'タイトル'
+admin.site.site_title = '商品管理アプリ'
 #  サイト名：ログイン画面と管理画面上部の表示
-admin.site.site_header = 'サンプルアプリ'
+admin.site.site_header = '商品管理アプリ'
 #  メニュー：管理画面の見出し表示
 admin.site.index_title = 'メニュー'
